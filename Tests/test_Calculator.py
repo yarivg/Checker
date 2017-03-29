ae257082ae5e9c2ca818292965d8df3233e2ad9a
yarivg <yarivgavriel2@gmail.com>
2017-03-29 21:50:01 +0300

    adding division method with test

--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -3,10 +3,14 @@ from Calculator import Calculator
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
-        self.assertEquals(Calculator.add(self, 5, 3), 8)
+        self.assertEquals(8, Calculator.add(self, 5, 3))
 
     def test_sub(self):
-        self.assertEquals(Calculator.sub(self, 5, 3), 2)
+        self.assertEquals(2, Calculator.sub(self, 5, 3))
+
+    def test_div(self):
+        self.assertEquals(2, Calculator.div(self, 6, 3))
+        self.assertRaises(ZeroDivisionError, lambda: Calculator.div(self, 5, 0))
 
 
 if __name__ == "__main__":
