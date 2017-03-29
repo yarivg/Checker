d7f97f4e622043515161fdd2247dedf384e93ffd
yarivg <yarivgavriel2@gmail.com>
2017-03-29 21:01:29 +0300

    adding self to class params

--- Calculator.py
+++ Calculator.py
@@ -1,15 +1,14 @@
 def main():
-	x = 5
-	y = 10
-	print(Calculator.add(x,y))
+    x = 5
+    y = 10
+    print(Calculator.add(x,y))
 
 class Calculator():
+    def add(self, x, y):
+        return x + y
 
-	def add(x, y):
-		return x + y
-
-	def sub(x, y):
-		return x - y
+    def sub(self, x, y):
+        return x - y
 
 if __name__ == "__main__":
     main()
--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -3,10 +3,10 @@ from Calculator import Calculator
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
-        self.assertEquals(Calculator.add(5, 3), 8)
+        self.assertEquals(Calculator.add(self, 5, 3), 8)
 
     def test_sub(self):
-        self.assertEquals(Calculator.sub(5, 3), 2)
+        self.assertEquals(Calculator.sub(self, 5, 3), 2)
 
 
 if __name__ == "__main__":
